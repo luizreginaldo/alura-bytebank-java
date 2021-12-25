cdcf802cb2d1c6434cf0a67d4686e0c4d573a48e
Luiz Reginaldo Silva <contato@luizreginaldo.com>
2021-12-25 20:06:02 -0300

    referência null

---
+++ src/TestaContaSemCliente.java
@@ -0,0 +1,17 @@
+
+public class TestaContaSemCliente {
+
+	public static void main(String[] args) {
+		
+		Conta contaA = new Conta();
+		System.out.println(contaA.saldo);
+		System.out.println(contaA.titular); //null
+//		System.out.println(contaA.titular.nome); //NullPointerException
+		
+		contaA.titular = new Cliente();
+		System.out.println(contaA.titular);
+		
+		contaA.titular.nome = "Cliente A";
+		System.out.println(contaA.titular.nome);
+	}
+}

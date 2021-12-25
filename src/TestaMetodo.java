6e6236218d6dea5b81474abbba6ea27ebe408984
Luiz Reginaldo Silva <contato@luizreginaldo.com>
2021-12-25 19:22:00 -0300

    métodos com referência e mais retorno

--- src/TestaMetodo.java
+++ src/TestaMetodo.java
@@ -3,13 +3,25 @@ public class TestaMetodo {
 
 	public static void main(String[] args) {
 		
-		Conta conta = new Conta();
-		conta.saldo = 100;
-		conta.deposita(50);
-		System.out.println(conta.saldo);
+		Conta contaA = new Conta();
+		contaA.saldo = 100;
+		contaA.deposita(50);
+		System.out.println(contaA.saldo);
 		
-		boolean conseguiuRetirar = conta.saca(20);
-		System.out.println(conta.saldo);
+		boolean conseguiuRetirar = contaA.saca(20);
+		System.out.println(contaA.saldo);
 		System.out.println(conseguiuRetirar);
+		
+		Conta contaB = new Conta();
+		contaB.deposita(1000);
+		boolean sucessoTransferencia = contaB.transfere(300, contaA);
+		if(sucessoTransferencia) {
+			System.out.println("transferência com sucesso.");
+		} else {
+			System.out.println("faltou dinheiro");
+		}
+		System.out.println("saldo contaA: " + contaA.saldo);
+		System.out.println("saldo contaB: " + contaB.saldo);
+		
 	}
 }

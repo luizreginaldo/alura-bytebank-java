4f6ea40b8ba7fe4b8d924ed1d9d3d561dd148c6d
Luiz Reginaldo Silva <contato@luizreginaldo.com>
2021-12-26 15:36:59 -0300

    getters e setters de referências

--- src/TestaBanco.java
+++ src/TestaBanco.java
@@ -3,14 +3,14 @@ public class TestaBanco {
 
 	public static void main(String[] args) {
 		Cliente clienteA = new Cliente();
-		clienteA.nome = "Cliente A";
-		clienteA.cpf = "123.456.789-10";
-		clienteA.profissao = "Engenheiro";
+		clienteA.setNome("Cliente A");
+		clienteA.setCpf("123.456.789-10");
+		clienteA.setProfissao("Engenheiro");
 		
 		Conta contaA = new Conta();
 		contaA.deposita(100);
 		contaA.setTitular(clienteA);
-		System.out.println(contaA.getTitular().nome);
+		System.out.println(contaA.getTitular().getNome());
 		System.out.println(contaA.getSaldo());
 	}
 }
--- src/TestaContaSemCliente.java
+++ src/TestaContaSemCliente.java
@@ -6,12 +6,12 @@ public class TestaContaSemCliente {
 		Conta contaA = new Conta();
 		System.out.println(contaA.getSaldo());
 		System.out.println(contaA.getTitular()); //null
-//		System.out.println(contaA.getTitular().nome); //NullPointerException
+//		System.out.println(contaA.getTitular().getNome()); //NullPointerException
 		
 		contaA.setTitular(new Cliente());
 		System.out.println(contaA.getTitular());
 		
-		contaA.getTitular().nome = "Cliente A";
-		System.out.println(contaA.getTitular().nome);
+		contaA.getTitular().setNome("Cliente A");
+		System.out.println(contaA.getTitular().getNome());
 	}
 }

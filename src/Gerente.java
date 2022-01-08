e7dcc59cff586f45f2caf5d8cc154d8fe32a1612
Luiz Reginaldo Silva <contato@luizreginaldo.com>
2022-01-08 00:09:48 -0300

    completando o sistema

--- src/Gerente.java
+++ src/Gerente.java
@@ -1,9 +1,25 @@
 
-public class Gerente extends Funcionario {
+public class Gerente extends Funcionario implements Autenticavel {
+	
+	private int senha;
 	
 	@Override
 	public double getBonificacao() {
 		return super.getSalario();
+	}
+
+	@Override
+	public void setSenha(int senha) {
+		this.senha = senha;
+		
+	}
+
+	@Override
+	public boolean autentica(int senha) {
+		if(this.senha == senha) {
+			return true;
+		}
+		return false;
 	}
 	
 }

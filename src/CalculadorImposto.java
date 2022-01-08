8813caf3b8fc1720b8b654e403ee6b705207a17e
Luiz Reginaldo Silva <contato@luizreginaldo.com>
2022-01-08 10:57:11 -0300

    implementando tributáveis

---
+++ src/CalculadorImposto.java
@@ -0,0 +1,14 @@
+
+public class CalculadorImposto {
+	
+	private double totalImposto;
+	
+	public void registra(Tributavel tributavel) {
+		this.totalImposto += tributavel.getValorImposto();	
+	}
+	
+	public double getTotalImposto() {
+		return totalImposto;
+	}
+	
+}

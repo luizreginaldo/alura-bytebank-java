8bd874a1448671592b682a2bc65ce04039bd9e1a
Luiz Reginaldo Silva <contato@luizreginaldo.com>
2022-01-15 15:39:11 -0300

    checked e unchecked

--- src/FluxoExceptionsComError.java
+++ src/FluxoExceptionsComError.java
@@ -1,11 +1,11 @@
 
-public class FluxoExceptions {
+public class FluxoExceptionsComError {
 	public static void main(String[] args) {
 		System.out.println("Ini do main");
 		
 		try {
 			metodo1();
-		} catch (ArithmeticException | NullPointerException | MinhaExcecao exception) {
+		} catch (ArithmeticException | NullPointerException exception) {
 			String mensagem = exception.getMessage();
 			System.out.println("Exception " + mensagem);
 			exception.printStackTrace();
@@ -13,16 +13,15 @@ public class FluxoExceptions {
 		System.out.println("Fim do main");
 	}
 	
-	private static void metodo1() throws MinhaExcecao {
+	private static void metodo1() {
 		System.out.println("Ini do metodo1");
 		metodo2();
 		System.out.println("Fim do metodo1");
 	}
 	
-	private static void metodo2() throws MinhaExcecao {
+	private static void metodo2() {
 		System.out.println("Ini do metodo2");
-		throw new MinhaExcecao("deu muito errado");
-		
-//		System.out.println("Fim do metodo2");
+		metodo2();
+		System.out.println("Fim do metodo2");
 	}
 }

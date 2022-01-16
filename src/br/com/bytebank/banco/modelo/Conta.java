1b470ce75e1ba598af73abc62d2fadfed556ee95
Luiz Reginaldo Silva <contato@luizreginaldo.com>
2022-01-16 15:28:59 -0300

    conhecendo o javadoc

--- src/br/com/bytebank/banco/modelo/Conta.java
+++ src/br/com/bytebank/banco/modelo/Conta.java
@@ -1,5 +1,11 @@
 package br.com.bytebank.banco.modelo;
 
+/**
+ * Classe representa a moldura de uma conta
+ * 
+ * @author Luiz Reginaldo
+ *
+ */
 public abstract class Conta {
 	
 	protected double saldo;
@@ -8,6 +14,12 @@ public abstract class Conta {
 	private Cliente titular;
 	private static int total;
 	
+	/**
+	 * Construtor para inicializar o objeto Conta a partir da agência e número.
+	 * 
+	 * @param agencia
+	 * @param numero
+	 */
 	public Conta(int agencia, int numero) {
 		Conta.total++;
 
@@ -21,6 +33,12 @@ public abstract class Conta {
 	
 	public abstract void deposita(double valor);
 	
+	/**
+	 * Valor precisa ser maior do que o saldo.
+	 * 
+	 * @param valor
+	 * @throws SaldoInsuficienteException
+	 */
 	public void saca(double valor) throws SaldoInsuficienteException {
 		if(this.saldo < valor) {
 			throw new SaldoInsuficienteException("Saldo : " + this.saldo + ", Valor: " + valor);

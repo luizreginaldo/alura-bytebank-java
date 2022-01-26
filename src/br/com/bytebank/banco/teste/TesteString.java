63e3f7f9e0f21819127da835fd5685568b2a8508
Luiz Reginaldo Silva <contato@luizreginaldo.com>
2022-01-26 09:53:00 -0300

    Metodos da classe String

--- src/br/com/bytebank/banco/teste/TesteString.java
+++ src/br/com/bytebank/banco/teste/TesteString.java
@@ -4,13 +4,39 @@ public class TesteString {
 
 	public static void main(String[] args) {
 		
-		String nome = "Alura"; //object literal
-//		String outro = new String("Alura");
+		String vazio = " ";
+		System.out.println(vazio.isEmpty());
+		System.out.println(vazio.trim().isEmpty());
 		
-		nome = nome.replace("A", "a");
-		System.out.println(nome);
-		nome = nome.toUpperCase();
-		System.out.println(nome);
+		String nome = "Alura"; //object literal
+		
+		System.out.println(nome.contains("lu"));
+		
+		String outro = new String("Alura");
+		
+		System.out.println(nome.length());
+		
+		for(int i = 0; i < nome.length(); i++) {
+			System.out.println(nome.charAt(i));
+		}
+		
+		String substring = nome.substring(1);
+		System.out.println(substring);
+		
+		int indexOf = nome.indexOf("ur");
+		System.out.println(indexOf);
+		
+		char myChar = nome.charAt(0);
+		System.out.println(myChar);
+		
+		String replace = nome.replace("Al", "aL");
+		System.out.println(replace);
+		
+		String toUpperCase = nome.toUpperCase();
+		System.out.println(toUpperCase);
+		
+		String replaceChar = nome.replace('l', 'L');
+		System.out.println(replaceChar);
 		
 	}
 
